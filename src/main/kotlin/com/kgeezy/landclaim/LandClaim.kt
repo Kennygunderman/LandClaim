8efdfcd9e2d884b3b88ffae5dd40bf35ae831f9c
Kenny <kennygunderman@gmail.com>
2019-07-25 23:33:31 -0400

    initial plugin setup

---
+++ src/main/kotlin/com/kgeezy/landclaim/LandClaim.kt
@@ -0,0 +1,20 @@
+package com.kgeezy.landclaim
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.plugin.java.JavaPlugin
+
+class LandClaim: JavaPlugin() {
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        return super.onCommand(sender, command, label, args)
+    }
+}
